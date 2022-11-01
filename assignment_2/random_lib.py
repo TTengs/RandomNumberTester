331391ebcd21f91f7de6ca312061d07cc3b67a0d
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-01 12:25:40 +0100

    added random library to generate random integers between 0 and 1 in python

---
+++ assignment_2/random_lib.py
@@ -0,0 +1,16 @@
+"""Random library that uses random function to generate random numbers between [0,1)"""
+import random
+
+
+def random_lib():
+    """python random library"""
+    i = 0
+    rn_list = []
+    while i < 10000:
+        random_double = random.random()
+        rn_list.append(random_double)
+        i = i + 1
+    print(rn_list)
+
+
+random_lib()
