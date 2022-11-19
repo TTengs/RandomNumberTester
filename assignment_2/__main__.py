b20bb4acbfc8164209ac85d1b5af0189726b46c6
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-19 13:51:30 +0100

    testing to reassute correctness of results

--- assignment_2/__main__.py
+++ assignment_2/__main__.py
@@ -8,12 +8,12 @@ from tests import chi_sq_test
 if __name__ == '__main__':
     """DO KS_TEST"""
 
-    print("----A linear congruential generator---")
-    ks_test.k_s(lcm_gen.rn[:100])
-    print("---RANDU generator---")
-    ks_test.k_s(lcm_randu.rando_list[:100])
-    print("---python library generator---")
-    ks_test.k_s(random_lib.random_list[:100])
+    # print("----A linear congruential generator---")
+    # ks_test.k_s(lcm_gen.rn[:100])
+    # print("---RANDU generator---")
+    # ks_test.k_s(lcm_randu.rando_list[:100])
+    # print("---python library generator---")
+    # ks_test.k_s(random_lib.random_list[:100])
 
     """DO CHI_SQUARE TEST"""
 
