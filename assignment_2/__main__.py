24c56ec43257330956180e1129803f3c7afc4744
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-26 14:40:07 +0100

    fixed i so results are correct

--- assignment_2/__main__.py
+++ assignment_2/__main__.py
@@ -1,12 +1,21 @@
 """main file to run. It produces results whethers tests are rejected or not"""
 import lcm_gen
 import lcm_randu
-# import random_lib
+import random_lib
 # from tests import ks_test
 # from tests import chi_sq_test
-from tests import run_test
+# from tests import run_test
 from tests import corollation_test
 
 if __name__ == '__main__':
 
+    print("lcm generator without randu ")
     corollation_test.corollation_test(lcm_gen.rn)
+
+    print(" ")
+    print("lcm generator with randu ")
+    corollation_test.corollation_test(lcm_randu.rando_list)
+
+    print(" ")
+    print("lcm generator using the library ")
+    corollation_test.corollation_test(random_lib.random_list)
--- assignment_2/tests/corollation_test.py
+++ assignment_2/tests/corollation_test.py
@@ -10,17 +10,26 @@ def corollation_test(the_random_list):
     _random = the_random_list
 
     N = 10000
-    i = 3
+    i = 2
     m = 128
 
     # get M <= (N-i-m)/m
     M = get_M(N, i, m)
     rho = compute_rho(i, M, m, _random)
     print("rho is : ", rho)
+
     sigma = compute_sigma(M)
     print("sigma : ", sigma)
+
     Z = compute_Z(rho, sigma)
     print("Z is  : ", Z)
+    
+    isNotRejected = isHypothesisNotRejected(Z)
+    if(isNotRejected):
+        print("We do not reject the null hypothesis")
+    else:
+        print("We reject the null hypothesis")
+
 
 
 def get_M(N, i, m):
@@ -37,11 +46,10 @@ def compute_rho(i, M, m, _random):
     while k <= M:
         R1 = _random[i+(k*m)]
         R2 = _random[i+(k+1)*m]
-        the_list.append((R1 * R2)/(M+1))
+        the_list.append(R1 * R2)
         k += 1
     the_list_sum = sum(the_list)
-    print("the list sum is : ", the_list_sum)
-    return (the_list_sum - 0.25)
+    return (the_list_sum / (M+1) - 0.25)
 
 
 def compute_sigma(M):
@@ -50,3 +58,7 @@ def compute_sigma(M):
 
 def compute_Z(rho, sigma):
     return rho/sigma
+
+def isHypothesisNotRejected(Z):
+    notRejected = -1.96 < Z < 1.96
+    return notRejected
