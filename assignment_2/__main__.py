1f55a7f55d752012d3a422523d940efc43468fe3
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-26 14:54:20 +0100

    added some separators between the tests

--- assignment_2/__main__.py
+++ assignment_2/__main__.py
@@ -4,7 +4,7 @@ import lcm_randu
 import random_lib
 # from tests import ks_test
 # from tests import chi_sq_test
-# from tests import run_test
+from tests import run_test
 from tests import corollation_test
 
 if __name__ == '__main__':
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     print(" ")
     print("---python library generator---")
     run_test.run_test(random_lib.random_list)
+
+    print("")
     print("lcm generator without randu ")
     corollation_test.corollation_test(lcm_gen.rn)
 
