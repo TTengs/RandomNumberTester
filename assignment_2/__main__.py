1c00e67cf3749b23bc9b411dc0e3a0b48ec28f25
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-26 14:56:48 +0100

    added separators between the tests of ks

--- assignment_2/__main__.py
+++ assignment_2/__main__.py
@@ -5,9 +5,14 @@ import random_lib
 from tests import ks_test
 
 if __name__ == '__main__':
+
     print("----A linear congruential generator---")
     ks_test.k_s(lcm_gen.rn[:100])
+
+    print(" ")
     print("---RANDU generator---")
     ks_test.k_s(lcm_randu.rando_list[:100])
+
+    print(" ")
     print("---python library generator---")
     ks_test.k_s(random_lib.random_list[:100])
