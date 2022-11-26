3ddd0e0f9eace7c1bb673e7dbd6f0b0df7dcc0d3
Abdul karim Yassin <karim.mmd12@gmail.com>
2022-11-26 15:09:40 +0100

    added separators

--- assignment_2/__main__.py
+++ assignment_2/__main__.py
@@ -2,8 +2,8 @@
 import lcm_gen
 import lcm_randu
 import random_lib
-# from tests import ks_test
-# from tests import chi_sq_test
+from tests import ks_test
+from tests import chi_sq_test
 from tests import run_test
 from tests import corollation_test
 
@@ -11,7 +11,11 @@ if __name__ == '__main__':
 
     print("----A linear congruential generator---")
     chi_sq_test.chi_test(lcm_gen.rn)
+
+    print("")
     print("---RANDU generator---")
     chi_sq_test.chi_test(lcm_randu.rando_list)
+
+    print("")
     print("---python library generator---")
     chi_sq_test.chi_test(random_lib.random_list)
